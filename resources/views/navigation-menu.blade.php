b4e1832bb499516e94e72ace87c18a63631ff99b
David Cruz <info@davidcruz.net>
2024-11-07 12:51:06 +0100

    add products to navigation menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('product.index') }}" :active="request()->routeIs('product.index')">
+                        {{ __('Products') }}
+                    </x-nav-link>
                 </div>
             </div>
 
